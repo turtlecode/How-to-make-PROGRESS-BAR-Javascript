18b5ff6ab5599f82adc565c9a1ef837ab5c9c355
turtlecode <85156399+turtlecode@users.noreply.github.com>
2022-01-30 10:46:57 +0300

    How to make PROGRESS BAR - Javascript
    How to make PROGRESS BAR - Javascript

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
+var i = 0;
 
-const newYears = "1 Jan 2023";
-
-function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    elem.style.visibility = "visible";
+    var width = 10;
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
 }
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-}
-
-// initial call
-countdown();
-
-setInterval(countdown, 1000);
